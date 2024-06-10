ed6fb19b7be5b720845a8807c22da63f7c380d48
SOURAV GARAI <101336956+Garaisourav12@users.noreply.github.com>
2024-06-10 22:52:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,54 @@
+//your JS code here. If required.
+// const textElement = document.getElementById("text");
+// const speedInput = document.getElementById("speed");
+// const startButton = document.getElementById("startButton");
+
+// let intervalId;
+
+// startButton.addEventListener("click", function () {
+//     clearInterval(intervalId);
+
+//     const speed = parseInt(speedInput.value);
+//     const text = textElement.innerText;
+//     textElement.innerText = ''
+//     if (speed >= 1 && speed <= 10) {
+//         const delay = 500 / speed;
+//         let i = 0;
+
+//         intervalId = setInterval(function () {
+//             if (i < text.length) {
+//                 textElement.textContent += text[i];
+//                 i++;
+//             } else {
+//                 clearInterval(intervalId);
+//             }
+//         }, delay);
+//     } else {
+//         alert("Please enter a speed between 1 and 10.");
+//     }
+// });
+
 const text = document.getElementById('text');
-const speedInput = document.getElementById('speed');
-const originalText = 'We love Programming!';
-let currentIndex = 1;
+      const speedInput = document.getElementById('speed');
+      const originalText = 'We love Programming!';
+      let currentIndex = 0;
 
-// Set initial text content
-text.innerHTML = "W";
+      function typeNextCharacter() {
+        if (currentIndex < originalText.length) {
+          const currentCharacter = originalText.charAt(currentIndex);
+          text.innerHTML += currentCharacter;
+          currentIndex++;
+          setTimeout(typeNextCharacter, 500 / parseInt(speedInput.value));
+        }
+      }
 
-function typeNextCharacter() {
-  if (currentIndex < originalText.length) {
-    const currentCharacter = originalText.charAt(currentIndex);
-    text.innerHTML = originalText.substring(0, currentIndex) + currentCharacter;
-    currentIndex++;
-    setTimeout(typeNextCharacter, 500 / parseInt(speedInput.value));
-  }
-}
+      speedInput.addEventListener('input', () => {
+        // Reset the text and index when the speed changes
+        text.innerHTML = '';
+        currentIndex = 0;
+        // Start typing again with the new speed
+        typeNextCharacter();
+      });
 
-speedInput.addEventListener('input', () => {
-  // Reset the index when the speed changes
-  currentIndex = 0;
-  // Start typing again with the new speed
-  typeNextCharacter();
-});
-
-// Start typing the text with the initial speed
-typeNextCharacter();
+      // Start typing the text with the initial speed
+      typeNextCharacter();
